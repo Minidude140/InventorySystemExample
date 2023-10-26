ad60fa0c97c56d38c87e9ae2e4b216091654a36e
Zac <zac.minidude140@Gmail.com>
2023-10-26 08:37:34 -0600

    Add project files.

---
+++ InventorySystemExample/Form1.Designer.vb
@@ -0,0 +1,330 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RecordGroupBox = New System.Windows.Forms.GroupBox()
+        Me.DataSheetLabel = New System.Windows.Forms.Label()
+        Me.ManufactureLabel = New System.Windows.Forms.Label()
+        Me.ManufactureTextBox = New System.Windows.Forms.TextBox()
+        Me.DataSheetTextBox = New System.Windows.Forms.TextBox()
+        Me.VenderLabel = New System.Windows.Forms.Label()
+        Me.LocationLabel = New System.Windows.Forms.Label()
+        Me.PartNumberLabel = New System.Windows.Forms.Label()
+        Me.DescriptionLabel = New System.Windows.Forms.Label()
+        Me.PartNumberTextBox = New System.Windows.Forms.TextBox()
+        Me.LocationTextBox = New System.Windows.Forms.TextBox()
+        Me.VendorTextBox = New System.Windows.Forms.TextBox()
+        Me.DescriptionTextBox = New System.Windows.Forms.TextBox()
+        Me.OptionsGroupBox = New System.Windows.Forms.GroupBox()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
+        Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.RecordGroupBox.SuspendLayout()
+        Me.OptionsGroupBox.SuspendLayout()
+        Me.ButtonGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RecordGroupBox
+        '
+        Me.RecordGroupBox.Controls.Add(Me.DataSheetLabel)
+        Me.RecordGroupBox.Controls.Add(Me.ManufactureLabel)
+        Me.RecordGroupBox.Controls.Add(Me.ManufactureTextBox)
+        Me.RecordGroupBox.Controls.Add(Me.DataSheetTextBox)
+        Me.RecordGroupBox.Controls.Add(Me.VenderLabel)
+        Me.RecordGroupBox.Controls.Add(Me.LocationLabel)
+        Me.RecordGroupBox.Controls.Add(Me.PartNumberLabel)
+        Me.RecordGroupBox.Controls.Add(Me.DescriptionLabel)
+        Me.RecordGroupBox.Controls.Add(Me.PartNumberTextBox)
+        Me.RecordGroupBox.Controls.Add(Me.LocationTextBox)
+        Me.RecordGroupBox.Controls.Add(Me.VendorTextBox)
+        Me.RecordGroupBox.Controls.Add(Me.DescriptionTextBox)
+        Me.RecordGroupBox.Location = New System.Drawing.Point(88, 53)
+        Me.RecordGroupBox.Name = "RecordGroupBox"
+        Me.RecordGroupBox.Size = New System.Drawing.Size(319, 254)
+        Me.RecordGroupBox.TabIndex = 4
+        Me.RecordGroupBox.TabStop = False
+        Me.RecordGroupBox.Text = "Record"
+        '
+        'DataSheetLabel
+        '
+        Me.DataSheetLabel.AutoSize = True
+        Me.DataSheetLabel.Location = New System.Drawing.Point(9, 172)
+        Me.DataSheetLabel.Name = "DataSheetLabel"
+        Me.DataSheetLabel.Size = New System.Drawing.Size(75, 17)
+        Me.DataSheetLabel.TabIndex = 11
+        Me.DataSheetLabel.Text = "DataSheet"
+        '
+        'ManufactureLabel
+        '
+        Me.ManufactureLabel.AutoSize = True
+        Me.ManufactureLabel.Location = New System.Drawing.Point(6, 142)
+        Me.ManufactureLabel.Name = "ManufactureLabel"
+        Me.ManufactureLabel.Size = New System.Drawing.Size(87, 17)
+        Me.ManufactureLabel.TabIndex = 10
+        Me.ManufactureLabel.Text = "Manufacture"
+        '
+        'ManufactureTextBox
+        '
+        Me.ManufactureTextBox.Location = New System.Drawing.Point(95, 139)
+        Me.ManufactureTextBox.Name = "ManufactureTextBox"
+        Me.ManufactureTextBox.Size = New System.Drawing.Size(213, 22)
+        Me.ManufactureTextBox.TabIndex = 8
+        '
+        'DataSheetTextBox
+        '
+        Me.DataSheetTextBox.Location = New System.Drawing.Point(95, 167)
+        Me.DataSheetTextBox.Name = "DataSheetTextBox"
+        Me.DataSheetTextBox.Size = New System.Drawing.Size(213, 22)
+        Me.DataSheetTextBox.TabIndex = 9
+        '
+        'VenderLabel
+        '
+        Me.VenderLabel.AutoSize = True
+        Me.VenderLabel.Location = New System.Drawing.Point(19, 114)
+        Me.VenderLabel.Name = "VenderLabel"
+        Me.VenderLabel.Size = New System.Drawing.Size(54, 17)
+        Me.VenderLabel.TabIndex = 7
+        Me.VenderLabel.Text = "Vendor"
+        '
+        'LocationLabel
+        '
+        Me.LocationLabel.AutoSize = True
+        Me.LocationLabel.Location = New System.Drawing.Point(19, 86)
+        Me.LocationLabel.Name = "LocationLabel"
+        Me.LocationLabel.Size = New System.Drawing.Size(62, 17)
+        Me.LocationLabel.TabIndex = 6
+        Me.LocationLabel.Text = "Location"
+        '
+        'PartNumberLabel
+        '
+        Me.PartNumberLabel.AutoSize = True
+        Me.PartNumberLabel.Location = New System.Drawing.Point(27, 56)
+        Me.PartNumberLabel.Name = "PartNumberLabel"
+        Me.PartNumberLabel.Size = New System.Drawing.Size(46, 17)
+        Me.PartNumberLabel.TabIndex = 5
+        Me.PartNumberLabel.Text = "Part #"
+        '
+        'DescriptionLabel
+        '
+        Me.DescriptionLabel.AutoSize = True
+        Me.DescriptionLabel.Location = New System.Drawing.Point(9, 25)
+        Me.DescriptionLabel.Name = "DescriptionLabel"
+        Me.DescriptionLabel.Size = New System.Drawing.Size(79, 17)
+        Me.DescriptionLabel.TabIndex = 4
+        Me.DescriptionLabel.Text = "Description"
+        '
+        'PartNumberTextBox
+        '
+        Me.PartNumberTextBox.Location = New System.Drawing.Point(95, 53)
+        Me.PartNumberTextBox.Name = "PartNumberTextBox"
+        Me.PartNumberTextBox.Size = New System.Drawing.Size(213, 22)
+        Me.PartNumberTextBox.TabIndex = 1
+        '
+        'LocationTextBox
+        '
+        Me.LocationTextBox.Location = New System.Drawing.Point(95, 81)
+        Me.LocationTextBox.Name = "LocationTextBox"
+        Me.LocationTextBox.Size = New System.Drawing.Size(213, 22)
+        Me.LocationTextBox.TabIndex = 2
+        '
+        'VendorTextBox
+        '
+        Me.VendorTextBox.Location = New System.Drawing.Point(95, 109)
+        Me.VendorTextBox.Name = "VendorTextBox"
+        Me.VendorTextBox.Size = New System.Drawing.Size(213, 22)
+        Me.VendorTextBox.TabIndex = 3
+        '
+        'DescriptionTextBox
+        '
+        Me.DescriptionTextBox.Location = New System.Drawing.Point(95, 25)
+        Me.DescriptionTextBox.Name = "DescriptionTextBox"
+        Me.DescriptionTextBox.Size = New System.Drawing.Size(213, 22)
+        Me.DescriptionTextBox.TabIndex = 0
+        '
+        'OptionsGroupBox
+        '
+        Me.OptionsGroupBox.Controls.Add(Me.RadioButton6)
+        Me.OptionsGroupBox.Controls.Add(Me.RadioButton5)
+        Me.OptionsGroupBox.Controls.Add(Me.CheckBox3)
+        Me.OptionsGroupBox.Controls.Add(Me.CheckBox2)
+        Me.OptionsGroupBox.Controls.Add(Me.CheckBox1)
+        Me.OptionsGroupBox.Controls.Add(Me.RadioButton4)
+        Me.OptionsGroupBox.Location = New System.Drawing.Point(413, 46)
+        Me.OptionsGroupBox.Name = "OptionsGroupBox"
+        Me.OptionsGroupBox.Size = New System.Drawing.Size(243, 224)
+        Me.OptionsGroupBox.TabIndex = 5
+        Me.OptionsGroupBox.TabStop = False
+        Me.OptionsGroupBox.Text = "Options"
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(62, 184)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton6.TabIndex = 12
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "RadioButton6"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(62, 157)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton5.TabIndex = 11
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "RadioButton5"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(77, 77)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(100, 21)
+        Me.CheckBox3.TabIndex = 9
+        Me.CheckBox3.Text = "CheckBox3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(77, 50)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(100, 21)
+        Me.CheckBox2.TabIndex = 8
+        Me.CheckBox2.Text = "CheckBox2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(77, 23)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(100, 21)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(62, 130)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton4.TabIndex = 10
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "RadioButton4"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'ButtonGroupBox
+        '
+        Me.ButtonGroupBox.Controls.Add(Me.UpdateButton)
+        Me.ButtonGroupBox.Controls.Add(Me.ClearButton)
+        Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
+        Me.ButtonGroupBox.Location = New System.Drawing.Point(148, 305)
+        Me.ButtonGroupBox.Name = "ButtonGroupBox"
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(463, 89)
+        Me.ButtonGroupBox.TabIndex = 6
+        Me.ButtonGroupBox.TabStop = False
+        '
+        'UpdateButton
+        '
+        Me.UpdateButton.Location = New System.Drawing.Point(6, 21)
+        Me.UpdateButton.Name = "UpdateButton"
+        Me.UpdateButton.Size = New System.Drawing.Size(144, 57)
+        Me.UpdateButton.TabIndex = 13
+        Me.UpdateButton.Text = "&Update"
+        Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(156, 21)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(144, 57)
+        Me.ClearButton.TabIndex = 14
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(307, 21)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(144, 57)
+        Me.ExitButton.TabIndex = 15
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RecordGroupBox)
+        Me.Controls.Add(Me.OptionsGroupBox)
+        Me.Controls.Add(Me.ButtonGroupBox)
+        Me.Name = "Form1"
+        Me.Text = "Inventory Manager"
+        Me.RecordGroupBox.ResumeLayout(False)
+        Me.RecordGroupBox.PerformLayout()
+        Me.OptionsGroupBox.ResumeLayout(False)
+        Me.OptionsGroupBox.PerformLayout()
+        Me.ButtonGroupBox.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RecordGroupBox As GroupBox
+    Friend WithEvents DataSheetLabel As Label
+    Friend WithEvents ManufactureLabel As Label
+    Friend WithEvents ManufactureTextBox As TextBox
+    Friend WithEvents DataSheetTextBox As TextBox
+    Friend WithEvents VenderLabel As Label
+    Friend WithEvents LocationLabel As Label
+    Friend WithEvents PartNumberLabel As Label
+    Friend WithEvents DescriptionLabel As Label
+    Friend WithEvents PartNumberTextBox As TextBox
+    Friend WithEvents LocationTextBox As TextBox
+    Friend WithEvents VendorTextBox As TextBox
+    Friend WithEvents DescriptionTextBox As TextBox
+    Friend WithEvents OptionsGroupBox As GroupBox
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents ButtonGroupBox As GroupBox
+    Friend WithEvents UpdateButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ InventorySystemExample/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("InventorySystemExample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("InventorySystemExample")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f26bf2f9-5dc6-4362-b24f-4fa77b7d059a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
